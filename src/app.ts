a8ce8d9ea573527a51b2a9b7d060226d800ede60
Ivaylo Davidov <ivaylo.davidov@amusnet.com>
2022-12-12 17:32:29 +0200

    Add Labels

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,56 @@
 
-const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
-
-let WIDHT = canvas.width;
-let HEIGHT = canvas.height;
-
 const LIGHT = 'rgb(255, 206, 158)';
 const DARK = 'rgb(209, 139, 71)';
 
-type ColorType = 'light' | 'dark';
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
 
+const WIDHT = canvas.width;
+const HEIGHT = canvas.height;
+
+type ColorType = 'light' | 'dark';
 
 grid();
 frame();
 smiley();
 checker();
+labels();
 
+
+function labels() {
+    const textLabel = 'abcdefgh';
+    const numberLabel = '87654321';
+
+    let firstTextX = 125;
+    let firstTextY = 75;
+
+    let secTextX = 125;
+    let secTextY = 525;
+
+    let firstNumX = 75;
+    let firstNumY = 125;
+
+    let secNumX = 525;
+    let secNumY = 125
+
+    ctx.beginPath();
+    ctx.font = '24px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    for (let i = 0; i < textLabel.length; i++) {
+        ctx.fillText(`${textLabel[i]}`, firstTextX, firstTextY);
+        ctx.fillText(`${textLabel[i]}`, secTextX, secTextY);
+        ctx.fillText(`${numberLabel[i]}`, firstNumX, firstNumY);
+        ctx.fillText(`${numberLabel[i]}`, secNumX, secNumY);
+        firstTextX += 50;
+
+        secTextX += 50;
+        firstNumY += 50;
+        secNumY += 50;
+    }
+    ctx.closePath();
+}
 
 function checker() {
 
@@ -37,7 +72,7 @@ function rect(file: number, rank: number, color: ColorType) {
     let style = '';
 
     if (color == 'dark') {
-        style = DARK;   
+        style = DARK;
     } else if (color == 'light') {
         style = LIGHT;
     }
